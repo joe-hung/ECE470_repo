b7889deed637188a786ec37a33ecf99bc9e8d899
kli56 <kli56@illinois.edu>
2024-09-18 09:21:53 -0500

    ece470_9/18

---
+++ build/lab2andDriver/drivers/gazebo_ros_pkgs/gazebo_ros/CMakeFiles/gazebo_ros_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/ur3/catkin_hyhung3_vayung2/devel/include/gazebo_ros/PhysicsConfig.h"
+  "/home/ur3/catkin_hyhung3_vayung2/devel/lib/python3/dist-packages/gazebo_ros/cfg/PhysicsConfig.py"
+  "/home/ur3/catkin_hyhung3_vayung2/devel/share/gazebo_ros/docs/PhysicsConfig-usage.dox"
+  "/home/ur3/catkin_hyhung3_vayung2/devel/share/gazebo_ros/docs/PhysicsConfig.dox"
+  "/home/ur3/catkin_hyhung3_vayung2/devel/share/gazebo_ros/docs/PhysicsConfig.wikidoc"
+  "CMakeFiles/gazebo_ros_gencfg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/gazebo_ros_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
